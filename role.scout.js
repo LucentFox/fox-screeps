625b411f45764656a8d79c287a0f1bc5ea1b8ebd
LucentFox <raziel.jatier@gmail.com>
2019-11-04 07:55:46 -0700

    tweak for per room populations

--- role.scout.js
+++ role.scout.js
@@ -2,26 +2,25 @@ var jobLogic = require('creep.jobs');
 var roomLogic = require('room.logic');
 
 const roleName = "scout";
+const optimalPopulation = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0};
+const creepBuilds = {
+    300: [CLAIM,MOVE,MOVE],
+    350: [CLAIM,MOVE,MOVE],
+    400: [CLAIM,MOVE,MOVE],
+    450: [CLAIM,MOVE,MOVE],
+    500: [CLAIM,MOVE,MOVE],
+    550: [CLAIM,MOVE,MOVE],
+    600: [CLAIM,MOVE,MOVE],
+    800: [CLAIM,MOVE,MOVE],
+    1300: [CLAIM,MOVE,MOVE],
+    1750: [CLAIM,MOVE,MOVE],
+    1800: [CLAIM,MOVE,MOVE]
+};
 
 var roleScout = {
     /** @param {Creep} creep **/
     populate: function(){
-        var scouts = _.filter(Game.creeps, (creep) => creep.memory.role === roleName);
-        
-        if(scouts.length < 0) {
-            var newName = roleName + Game.time;
-            var retval = 0;
-            
-            for(var name in Game.spawns){
-                var spawn = Game.spawns[name];
-                var roomInfo = roomLogic.getRoomInfo(spawn.room);
-                
-                if(roomInfo.roomLevel >= 3) {
-                    var retval = spawn.spawnCreep([CLAIM,MOVE,MOVE], newName, {memory: {role: roleName}});
-                }
-            }
-        }
-
+        roomLogic.spawnCreeps(roleName, optimalPopulation, creepBuilds, false, false);
     },
     activate: function(creep) {
         creep.say("🧭");
